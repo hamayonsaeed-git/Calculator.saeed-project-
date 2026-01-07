52edc5e6e861add83ee182567b4a04dc447e7042
hamayonsaeed-git <hamayonsaeed0327@gmail.com>
2026-01-08 00:24:43 +0500

    My first terminal update

---
+++ app.js
@@ -0,0 +1,40 @@
+let string = "";
+let input = document.querySelector(".calculator-screen");
+let buttons = document.querySelectorAll(".calculator-keys button");
+
+jbjwfwhf jcjb
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let value = button.value;        // for calculation
+    let display = button.innerText; // for showing on screen
+
+    if (value === "=") {
+      try {
+        string = eval(string);
+        input.value = string;
+        input.style.textAlign = "right";
+         input.style.animation = "none";     // reset
+        input.offsetHeight;                 // force reflow
+        input.style.animation = "resultUp 0.6s cubic-bezier(.22,1,.36,1)";
+        
+        
+      } catch {
+        string = "";
+        input.value = "Error";
+      }
+    }
+
+    else if (value === "all-clear") {
+      string = "";
+      input.value = "";
+      input.style.textAlign = "left"; 
+    }
+
+    else {
+      string += value;          
+      input.value += display;  //  screen shows ×
+    }
+  });
+});
+
